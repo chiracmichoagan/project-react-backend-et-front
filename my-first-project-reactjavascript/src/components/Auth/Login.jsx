a8b8ae25acf566ff56592ced1a0c13efc28d8d8a
Chirac Dona Michoagan <chirac.michoagan@chem.loc>
2025-01-21 11:23:00 +0100

    login , register ok sans le logout à revoir après

--- my-first-project-reactjavascript/src/components/Auth/Login.jsx
+++ my-first-project-reactjavascript/src/components/Auth/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AuthService from "../../services/Todo/authService";
 import { Link } from "react-router-dom";
-
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -14,21 +13,22 @@ const Login = () => {
     setSuccessMessage("");
 
     try {
-      const data = await AuthService.login(email, password);
-      if (data.token) {
-        localStorage.setItem('token', data.token);
-        console.log("Connexion réussie :", data);
+      const response = await AuthService.login(email, password);
+      
+      if (response) {
+        console.log("Connexion réussie :", response);
         setSuccessMessage("Connexion réussie ! Redirection en cours...");
-        
-        // Redirection après 3 secondes
         setTimeout(() => {
           window.location.replace("/voir-todo");
         }, 3000);
       } else {
         setErrorMessage("Token non reçu, veuillez réessayer.");
       }
+      return response.data;
     } catch (error) {
-      setErrorMessage(error.response?.data?.message || "Erreur lors de la connexion");
+      setErrorMessage(
+        error.response?.data?.message || "Erreur lors de la connexion"
+      );
       console.error("Erreur lors de la connexion :", error);
     }
   };
@@ -83,8 +83,12 @@ const Login = () => {
                         Password
                       </label>
                     </div>
-                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-                    {successMessage && <p className="text-green-500">{successMessage}</p>}
+                    {errorMessage && (
+                      <p className="text-red-500">{errorMessage}</p>
+                    )}
+                    {successMessage && (
+                      <p className="text-green-500">{successMessage}</p>
+                    )}
                     <div className="relative">
                       <button className="bg-cyan-500 text-white rounded-md px-2 py-1">
                         Submit
@@ -106,10 +110,22 @@ const Login = () => {
                   <g fill="none" fillRule="evenodd">
                     <g transform="translate(-401.000000, -860.000000)">
                       <g transform="translate(401.000000, 860.000000)">
-                        <path d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24" fill="#FBBC05" />
-                        <path d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333" fill="#EB4335" />
-                        <path d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667" fill="#34A853" />
-                        <path d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24" fill="#4285F4" />
+                        <path
+                          d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
+                          fill="#FBBC05"
+                        />
+                        <path
+                          d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
+                          fill="#EB4335"
+                        />
+                        <path
+                          d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
+                          fill="#34A853"
+                        />
+                        <path
+                          d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
+                          fill="#4285F4"
+                        />
                       </g>
                     </g>
                   </g>
@@ -118,7 +134,8 @@ const Login = () => {
               </button>
             </div>
             <p>
-              Vous n'avez pas de compte ? <Link to={'/register'}>Inscrivez-vous ici</Link>
+              Vous n'avez pas de compte ?{" "}
+              <Link to={"/register"}>Inscrivez-vous ici</Link>
             </p>
           </div>
         </div>
--- my-first-project-reactjavascript/src/services/Todo/authService.ts
+++ my-first-project-reactjavascript/src/services/Todo/authService.ts
@@ -1,38 +1,67 @@
 import api from "../../axios/http-common";
 
+const setToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+const removeToken = () => {
+  localStorage.removeItem("token");
+};
+
 const login = async (email, password) => {
   try {
     const response = await api.post("auth/login", { email, password });
-    return response;
+    if (response.data.token) {
+      setToken(response.data.token);
+    }
+    return response.data;
   } catch (error) {
-    throw new Error(error.response?.data?.message || 'Erreur lors de la connexion');
+    throw new Error(
+      error.response?.data?.message || "Erreur lors de la connexion"
+    );
   }
 };
 
 const register = async (username, email, password) => {
   try {
-    const response = await api.post("auth/register", { username, email, password });
+    const response = await api.post("auth/register", {
+      username,
+      email,
+      password,
+    });
     console.log("Réponse d'inscription réussie :", response.data); // Message de succès
     return response.data; // Retourne uniquement les données de la réponse
   } catch (error) {
     console.error("Erreur lors de l'inscription :", error); // Journalise l'erreur
-    throw new Error(error.response?.data?.message || 'Erreur lors de l\'inscription');
+    throw new Error(
+      error.response?.data?.message || "Erreur lors de l'inscription"
+    );
   }
 };
 
 const logout = async () => {
   try {
-    const response = await api.post("auth/logout"); 
-    return response.data;
+    const response = await api.post("auth/logout");
+    removeToken();
+    return response;
   } catch (error) {
-    throw new Error(error.response?.data?.message || 'Erreur lors de la déconnexion');
+    throw new Error(
+      error.response?.data?.message || "Erreur lors de la déconnexion"
+    );
   }
-}
+};
 
 const AuthService = {
   login,
   register,
-  logout,   
+  logout,
+  getToken,
+  setToken,
+  removeToken,
 };
 
 export default AuthService;
